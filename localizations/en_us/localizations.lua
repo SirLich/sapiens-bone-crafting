341425b6a94b968d5a41d4ececd9f530e9a0d2f0
SirLich <lrkoehler77@gmail.com>
2023-04-09 15:37:24 +0200

    Work on polish

--- localizations/en_us/localizations.lua
+++ localizations/en_us/localizations.lua
@@ -4,7 +4,28 @@ localizations.values = {
     object_bone_broth = "Bone Broth",
     object_bone_broth_plural = "Bone Broth",
 
-    
+    object_bone_hatchet = "Great Bone Axe",
+    object_bone_hatchet_plural = "Great Bone Axes",
+
+    object_bone_chair = "Bone Chair",
+    object_bone_chair_plural = "Bone Chairs",
+    object_bone_chair_summary = "Sit comfortable on the bones of your enemies.",
+
+    object_giant_bone = "Giant Bone",
+    object_giant_bone_plural = "Giant Bones",
+
+    object_sheep_skull = "Sheep Skull",
+    object_sheep_skull_plural = "Sheep Skulls",
+
+    object_tusk = "Mammoth Tusk",
+    object_tusk_plural = "Mammoth Tusks",
+
+    object_wind_chimes = "Bone Windchimes",
+    object_wind_chimes_plural = "Bone Windchimes",
+
+    storage_bone_storage = "Bone Items",
+    storage_skull_storage = "Skulls"
+
 }
 
 return localizations
