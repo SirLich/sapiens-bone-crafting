34a5aab0f9980a2b6d682cd8d7ec97ff8fc3f82b
SirLich <lrkoehler77@gmail.com>
2023-04-15 00:09:58 +0200

    Polish

--- localizations/en_us/localizations.lua
+++ localizations/en_us/localizations.lua
@@ -1,12 +1,14 @@
 local localizations = {}
 
 localizations.values = {
+    -- OBJECTS
     object_bone_broth = "Bone Broth",
     object_bone_broth_plural = "Bone Broth",
     object_bone_broth_summary = "A gruesome stew, poor in nurishment.",
 
     object_bone_hatchet = "Great Bone Axe",
     object_bone_hatchet_plural = "Great Bone Axes",
+    object_bone_hatchet_summary = "Extremely efficient and mighty axe. Takes a long time to craft.",
 
     object_bone_mask = "Bone Mask",
     object_bone_mask_plural = "Bone Masks",
@@ -18,18 +20,23 @@ localizations.values = {
 
     object_giant_bone = "Giant Bone",
     object_giant_bone_plural = "Giant Bones",
+    object_giant_bone_summary = "An absurdly large bone, used for decoration.",
 
     object_sheep_skull = "Sheep Skull",
     object_sheep_skull_plural = "Sheep Skulls",
+    object_sheep_skull_summary = "A decorative sheep skull, expertly carved.",
 
     object_tusk = "Mammoth Tusk",
     object_tusk_plural = "Mammoth Tusks",
+    object_tusk_summary = "An decorative mammoth tusk, expertly carved.",
 
     object_wind_chimes = "Bone Windchimes",
     object_wind_chimes_plural = "Bone Windchimes",
 
-    storage_bone_storage = "Bone Items",
-    storage_skull_storage = "Skulls"
+    -- STORAGE AREAS
+    storage_large_bone_storage = "Large Bones",
+    storage_skull_storage = "Skulls",
+    storage_mask_storage = "Masks",
 
 }
 
