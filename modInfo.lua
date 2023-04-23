010b516921d57c33d541c652d814abe85695315a
SirLich <lrkoehler77@gmail.com>
2023-04-23 16:42:58 +0200

    Push 1.0.0

--- modInfo.lua
+++ modInfo.lua
@@ -1,10 +1,11 @@
 local modInfo = {
     name = "Bone Crafting",
-    description = "An expansion mod for Sapiens, introducing a collection of bone-crafted decorations.",
-    version = "0.0.1",
+    description = "An expansion mod for Sapiens, introducing a collection of bone-crafted decorations, tools, and buildables.",
+    version = "1.0.0",
     type = "world",
     developer = "SirLich",
-    loadOrder = 1
+    loadOrder = 1,
+    preview = "assets/preview.jpg"
 }
 
 return modInfo
